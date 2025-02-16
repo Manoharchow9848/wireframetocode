7008f42deb89af68c9056243f03a126f993c70ba
Manoharchow9848 <=manuchow1.767@gmail.com>
2025-02-16 16:08:26 +0530

    env secured

--- configs/firebaseConfig.tsx
+++ configs/firebaseConfig.tsx
@@ -9,13 +9,13 @@ import { getStorage } from 'firebase/storage'
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDkspGlMiZvdxpfHlv6bztgKgHuQtXYl0g",
-    authDomain: "wireframe-6a8c1.firebaseapp.com",
-    projectId: "wireframe-6a8c1",
-    storageBucket: "wireframe-6a8c1.firebasestorage.app",
-    messagingSenderId: "863303047922",
-    appId: "1:863303047922:web:f6734a6b4ed52655370e6b",
-    measurementId: "G-Y3BS63J8KR"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_FIREBASE_MESURMENT_ID
   };
 
 // Initialize Firebase
